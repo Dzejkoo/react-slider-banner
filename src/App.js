06c105d20d49662928136b4701ecde7224351d0c
Jakub Jóźwiak <jozwiak.ja@icloud.com>
2022-05-17 20:11:46 +0200

    Add box shadow to single img

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Lays from './images/Lays_logo.png';
+import McDonald from './images/McDonald_logo.png';
+import Nutella from './images/nutella_logo.png';
 
-function App() {
+import Slider from "react-slick";
+
+const arrLogo = [Lays, McDonald, Nutella, Lays, McDonald, Nutella]
+
+export default function App() {
+  const renderSlides = () =>
+    arrLogo.map(num => (
+      <div className="container">
+        <img src={num} />
+      </div>
+    ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Slider
+        dots={false}
+        slidesToShow={3}
+        slidesToScroll={1}
+        autoplay={true}
+        autoplaySpeed={3000}
+        responsive={[
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            initialSlide: 2
+          }
+        }
+      ]}
+      >
+        {renderSlides()}
+      </Slider>
     </div>
   );
 }
-
-export default App;
